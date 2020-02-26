d33d7fe103e2f4e9436e7d7ff68ee6b2f7530e89
macielmarques <macielmrqs@gmail.com>
2020-02-25 23:20:56 -0300

    tpl site

--- index.php
+++ index.php
@@ -1,22 +1,22 @@
-                                         <?php 
+<?php 
 
-require_once("vendor/                             autoload.php");
-                                                 
-use \Slim\Slim;
-use \Hcode\Page;
+	require_once("vendor/autoload.php");
+	                                                 
+	use \Slim\Slim;
+	use \Hcode\Page;
 
-$app = new \Slim\Slim();
+	$app = new \Slim\Slim();
 
-$app->config('debug', true);
+	$app->config('debug', true);
 
-$app->get('/', function() {
-    
-	$page = new Page();
+	$app->get('/', function() {
+	    
+		$page = new Page();
 
-	$page->setTpl("index");
+		$page->setTpl("index");
 
-});
+	});
 
-$app->run();
+	$app->run();
 
  ?>
